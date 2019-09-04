74eb4c9a5c079c0d9a851f1f24350bfefce40d6c
KromaLee <h694251068k@163.com>
2019-09-04 12:41:20 +0800

    a

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push
+# git push
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:kromalee/vuepress-demo.git master:gh-pages
