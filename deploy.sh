68f8cbae25efd7f1385c272bf30bf545fc665a7e
KromaLee <h694251068k@163.com>
2019-09-04 22:19:07 +0800

    d

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # 提供gitpush的环境
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:$PAGE_REPO.git master:gh-pages
-git push -f https://$GITHUB_TOKEN@$PAGE_REPO master:$PAGE_BRAN
+git push -f https://$GH_TOKEN@$PAGE_REPO master:$PAGE_BRAN
 
 cd -
