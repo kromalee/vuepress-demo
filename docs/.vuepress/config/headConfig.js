30cc71ba43310186a2ed8f68da800e2985ac3d50
kromalee <h694251068k@163.com>
2019-09-05 14:28:02 +0800

    go

--- docs/.vuepress/config/headConfig.js
+++ docs/.vuepress/config/headConfig.js
@@ -1,8 +1,8 @@
 // const secretKeyConf = require('./secretKeyConfig')
 module.exports = [
-    ['link', { rel: 'apple-touch-icon', href: 'apple-touch-icon.png' }],
-    ['link', { rel: 'icon', href: 'favicon-32x32.png' }],
-    ['link', { rel: 'manifest', href: 'manifest.json' }],
+    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#ccc' }],
     // ['meta', { name: 'google-site-verification', content: secretKeyConf.googleSearchConsole }],
 ];
